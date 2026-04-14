85d96241c4369e65db77b44e11989a4c24672ded
BarrosoJorge <jorgitobarroso@hotmail.com>
2026-04-13 22:50:35 -0600

    Punto 3 feature_engineering

--- 4_analisis/main.py
+++ 4_analisis/main.py
@@ -1,7 +1,12 @@
 import pandas as pd
 from preprocessing.individual_functions import *
 from preprocessing.processing_pipe import process_pipeline
-from semantic_expression.pipeline import pipe
+#from semantic_expression.pipeline import pipe
+from feature_engineering.vocabulary import build_vocabulary_from_clean
+from feature_engineering.features import run_feature_pipeline
+
+
+
 def preprocessing_complete(input_file):
     create_data_folders()
     spanish, english, mixed = process_pipeline(input_file)
@@ -12,7 +17,9 @@ def preprocessing_complete(input_file):
         print("⚠️  File generated for mixed results.")
     print("\nPipeline executed successfully.")
 def main():
-    ##preprocessing_complete("data/raw/huatulco.csv")
-    pipe()
+    preprocessing_complete("/home/jorge/6to/Visualizacion de la informacion/Turismo/huatulco.csv")
+    #pipe()
+    build_vocabulary_from_clean()
+    run_feature_pipeline()
 if __name__ == "__main__":
     main()
